b59e874e0b561eea1a248185391cfdbbadd90d0d
Beknazar Esenbek <beknazar.esenbek@fiware.org>
2023-07-25 14:54:31 +0600

    Object mapper was updated

--- src/test/java/io/github/wistefan/mapping/desc/pojos/MySubscriptionPojo.java
+++ src/test/java/io/github/wistefan/mapping/desc/pojos/MySubscriptionPojo.java
@@ -2,6 +2,7 @@ package io.github.wistefan.mapping.desc.pojos;
 
 import io.github.wistefan.mapping.annotations.*;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
@@ -18,6 +19,7 @@ public class MySubscriptionPojo {
 
 	private final URI id;
 
+	@Getter(onMethod = @__({@AttributeGetter(value = AttributeType.PROPERTY, targetName = "name")}))
 	@Setter(onMethod = @__({@AttributeSetter(value = AttributeType.PROPERTY, targetName = "name")}))
 	private String name;
 
@@ -35,24 +37,30 @@ public class MySubscriptionPojo {
 		return SUBSCRIPTION_TYPE;
 	}
 
+	@Getter(onMethod = @__({@AttributeGetter(value = AttributeType.PROPERTY, targetName = "expires")}))
 	@Setter(onMethod = @__({@AttributeSetter(value = AttributeType.PROPERTY, targetName = "expires",
 			targetClass = Instant.class)}))
 	private Instant expires;
 
+	@Getter(onMethod = @__({@AttributeGetter(value = AttributeType.PROPERTY, targetName = "q")}))
 	@Setter(onMethod = @__({@AttributeSetter(value = AttributeType.PROPERTY, targetName = "q")}))
 	private String q;
 
+	@Getter(onMethod = @__({@AttributeGetter(value = AttributeType.GEO_QUERY, targetName = "geoQ")}))
 	@Setter(onMethod = @__({@AttributeSetter(value = AttributeType.GEO_QUERY, targetName = "geoQ",
 			targetClass = MyGeoQueryProperty.class)}))
 	private MyGeoQueryProperty geoQ = new MyGeoQueryProperty();
 
+	@Getter(onMethod = @__({@AttributeGetter(value = AttributeType.ENTITY_INFO_LIST, targetName = "entities")}))
 	@Setter(onMethod = @__({@AttributeSetter(value = AttributeType.ENTITY_INFO_LIST, targetName = "entities",
 			targetClass = MyEntityInfoProperty.class)}))
 	private List<MyEntityInfoProperty> entities;
 
+	@Getter(onMethod = @__({@AttributeGetter(value = AttributeType.PROPERTY_SET, targetName = "watchedAttributes")}))
 	@Setter(onMethod = @__({@AttributeSetter(value = AttributeType.PROPERTY_SET, targetName = "watchedAttributes")}))
 	private Set<String> watchedAttributes;
 
+	@Getter(onMethod = @__({@AttributeGetter(value = AttributeType.NOTIFICATION_PARAMS, targetName = "notification")}))
 	@Setter(onMethod = @__({@AttributeSetter(value = AttributeType.NOTIFICATION_PARAMS, targetName = "notification",
 			targetClass = MyNotificationParamsProperty.class)}))
 	private MyNotificationParamsProperty notification = new MyNotificationParamsProperty();
