8a0faa20e208716976c5635e1560dc7c42d854a6
Beknazar Esenbek <beknazar.esenbek@fiware.org>
2023-09-13 10:18:33 +0200

    Added comment

--- src/main/java/io/github/wistefan/mapping/SubscriptionMixin.java
+++ src/main/java/io/github/wistefan/mapping/SubscriptionMixin.java
@@ -4,5 +4,10 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.fiware.ngsi.model.GeoQueryVO;
 
 public abstract class SubscriptionMixin {
+    /*
+      The reason to ignore this field for now is that TMForum API notifications don't have geo queries,
+      so it's done instead of writing custom deserializer for this field, because field types of geo query cannot be
+      uniquely deduced
+     */
     @JsonIgnore GeoQueryVO geoQ;
 }
