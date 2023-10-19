66aff9382de870b43c457f1c3500939446f14648
Beknazar Esenbek <beknazar.esenbek@fiware.org>
2023-10-19 14:21:00 +0200

    Removed redundant code

--- src/main/java/io/github/wistefan/mapping/CacheSerdeableObjectMapper.java
+++ src/main/java/io/github/wistefan/mapping/CacheSerdeableObjectMapper.java
@@ -1,8 +1,6 @@
 package io.github.wistefan.mapping;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
-import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
 import io.github.wistefan.mapping.mixins.AdditionalPropertyDiscriminatorMixin;
 import io.github.wistefan.mapping.mixins.AdditionalPropertyListDiscriminatorMixin;
 import io.github.wistefan.mapping.mixins.AdditionalPropertyObjectDiscriminatorMixin;
@@ -11,27 +9,11 @@ import jakarta.inject.Singleton;
 import org.fiware.ngsi.model.AdditionalPropertyListVO;
 import org.fiware.ngsi.model.AdditionalPropertyObjectVO;
 import org.fiware.ngsi.model.AdditionalPropertyVO;
-import org.fiware.ngsi.model.EntityVO;
-
-import java.net.URI;
-import java.time.Instant;
-import java.util.List;
 
 @Singleton
 @Bean
 public class CacheSerdeableObjectMapper extends ObjectMapper {
     public CacheSerdeableObjectMapper() {
-        PolymorphicTypeValidator ptv = BasicPolymorphicTypeValidator.builder()
-                .allowIfBaseType(EntityVO.class)
-                .allowIfBaseType(AdditionalPropertyVO.class)
-                .allowIfSubType(List.class)
-                .allowIfSubType(Instant.class)
-                .allowIfSubType(URI.class)
-                .allowIfSubType("io.github.wistefan.mapping.desc.pojos")
-                .allowIfSubType("org.fiware.tmforum.common.domain")
-                .build();
-        setPolymorphicTypeValidator(ptv)
-                .activateDefaultTyping(ptv, DefaultTyping.EVERYTHING);
         addMixIn(AdditionalPropertyVO.class, AdditionalPropertyDiscriminatorMixin.class);
         addMixIn(AdditionalPropertyObjectVO.class, AdditionalPropertyObjectDiscriminatorMixin.class);
         addMixIn(AdditionalPropertyListVO.class, AdditionalPropertyListDiscriminatorMixin.class);
